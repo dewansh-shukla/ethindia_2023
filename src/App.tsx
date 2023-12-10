1aafc20f6ca6fb67f10891218f9661018c6ec05e
Dewansh <89339943+dewansh-shukla@users.noreply.github.com>
2023-12-10 06:41:39 +0530

    Landing Page  (#3)
    Landing Page Improve
    
    ---------
    
    Co-authored-by: Nimisha Yadav <iamnimishayadav@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
+import mountainImage from "./mountains.jpg";
 import { ConnectKitButton } from "connectkit"
 import { motion } from "framer-motion"
 import { useAccount } from "wagmi"
 import { useNavigate } from "react-router-dom"
 import React from "react"
-const App = () => {
-  const { address } = useAccount()
-  const navigate = useNavigate()
-  React.useEffect(() => {
-    if (address) {
-    }
-  }, [address, navigate])
-  return (
-    <div className='min-h-screen bg-black text-white p-10'>
-      <h1 className='mb-6 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
-        Insurify
-      </h1>
-      <p className='text-lg  text-center mb-12'>
-        Welcome to our decentralized application (DApp) for Insurance Claim
-        Processing. Built on the Ethereum blockchain, our DApp leverages the
-        power of smart contracts to streamline the claims process, ensuring
-        transparency and efficiency.
-      </p>
 
+  const App = () => {
+    const { address } = useAccount()
+    const navigate = useNavigate()
+    const backgroundImageStyle = {
+      backgroundImage: `url("${mountainImage}")`,
+      backgroundSize: "cover",
+    }
+    React.useEffect(() => {
+      if (address) {
+      }
+    }, [address, navigate])
+  return (
+    <div className="App">
+      <div className=" text-white " style={backgroundImageStyle}>
+        <div className="bg-gradient-to-r from-black px-14 py-8">
+          <div className=" max-w-3xl grid grid-cols-1 gap-8">
+            <h2 className="text-xl uppercase font-bold">Insurify</h2>
+            <ConnectKitButton />
+            <h1 className="text-6xl font-bold">
+              Your Gateway to Easy Insurance Claims
+            </h1>
+            <p className="text-lg">
+              
+            Welcome to our decentralized application (DApp) for Insurance Claim Processing. Built on the Ethereum blockchain, our DApp leverages the power of smart contracts to streamline the claims process, ensuring transparency and efficiency. 
+             While the goal of medical insurance is to provide financial support during times of illness or injury, the complexity of the healthcare system, 
+            coupled with evolving regulations and administrative challenges, can make the medical insurance claim process difficult in today's time, which our application aims to rectify.
+            </p>
+            
+            <button className="bg-gradient-to-r from-pink-600 to-orange-600 py-3 px-6 text-lg rounded-md w-48">
+              See Dashboard
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="px-8 py-16">
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
         <motion.div
           className='bg-white text-black p-3 rounded border-2 border-purple-500'
@@ -66,45 +84,45 @@ const App = () => {
             </p>
           </motion.div>
         </div>
+        </div>
+        </div>
+      <div className="px-8 py-16">
+        <div className="max-w-md mb-16">
+          <h2 className="text-5xl">
+            Your next insurance claim made easier through Insurify.
+          </h2>
+        </div>
 
-        {/* Add other features here */}
-      </div>
-      <div className='mt-16'>
-        <ul className='timeline timeline-vertical'>
-          <li>
-            <div className='timeline-start timeline-box bg-black text-white border-1 border-purple-400'>
-              Enhanced Transparency
-            </div>
-            <hr className='bg-white m-2' />
-          </li>
-          <li>
-            <hr className='bg-white m-2' />
-            <div className='timeline-end timeline-box bg-black text-white border-1 border-purple-400'>
-              Improved Efficiency
-            </div>
-            <hr className='bg-white m-2' />
-          </li>
-          <li>
-            <hr className='bg-white m-2' />
-            <div className='timeline-start timeline-box bg-black text-white border-1 border-purple-400'>
-              Reduced Fraud
-            </div>
-            <hr className='bg-white m-2' />
-          </li>
-          <li>
-            <hr className='bg-white m-2' />
-            <div className='timeline-end timeline-box bg-black text-white border-1 border-purple-400'>
-              Lower Costs
-            </div>
-            <hr className='bg-white m-2' />
-          </li>
-        </ul>
-      </div>
-      <div className='w-full flex justify-center mt-20'>
-        <ConnectKitButton theme='auto' mode='dark' />
+        <div className="grid grid-cols-2 gap-4 text-slate-600">
+          <div>
+            <h3 className="text-2xl font-bold mb-2">Customer Satisfaction</h3>
+            <p className="text-lg">
+            The application aims to simplify procedures that can reduce the stress and hassle associated with filing a claim, making the overall experience more positive for policyholders.
+
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-bold mb-2">Competitive Advantage</h3>
+            <p className="text-lg">
+            A simple and efficient claims process can be a competitive differentiator for insurance companies in a crowded market. It can attract new customers and retain existing ones.            </p>
+          </div>
+          <div>
+            <h3 className="text-2xl font-bold mb-2">Risk Management</h3>
+            <p className="text-lg">
+            Quick settlements help policyholders recover from a loss or damage more promptly, reducing the financial impact and improving the overall risk management process.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-bold mb-2">Technology Integration</h3>
+            <p className="text-lg">
+            Leveraging technology for claims processing can lead to automation, reducing errors and speeding up the overall claims settlement process.                    </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
